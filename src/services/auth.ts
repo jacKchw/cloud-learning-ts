5d79a7a526563477f05cb51028b84487590336ad
Jack Chan <JackChanHW@connect.hku.hk>
2021-11-29 16:33:36 +0000

    Fix wrong token condition

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -10,13 +10,7 @@ export const usePassport = () => {
   passport.use(
     new Strategy(opts, function (jwt_payload, done) {
       try {
-        if (
-          jwt_payload.address &&
-          jwt_payload.email &&
-          jwt_payload.userid &&
-          jwt_payload.isPremium !== undefined &&
-          jwt_payload.roleType !== undefined
-        ) {
+        if (jwt_payload.userid && jwt_payload.username) {
           return done(null, jwt_payload)
         }
         return done(null, false)
